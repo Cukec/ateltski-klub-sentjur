e79a6090b7480782a7136863e1a42f0c63d85b2c
Gregor <g.gazvoda05@gmail.com>
2024-11-17 09:22:42 +0100

    domov
    druge slike (red background)

--- sites/galerija.php
+++ sites/galerija.php
@@ -7,10 +7,30 @@
     <link rel="stylesheet" href="styles/galerija.css">
     <!-- Cropper.js CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/cropperjs/dist/cropper.min.css">
+
+    <style>
+        .wrking{
+            height: 100%;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin-bottom: 10vh;
+            margin-top: -15vh;
+        }
+    </style>
 </head>
 <body>
     <?php include "navigation.php"; ?>
 
+    <div class="wrking">
+        <img src="../assets/working-on-it.png" height="35%" width="35%" alt="">
+        <h1 style="color: gray">Delamo na tem!</h1>
+    </div>
+    
+
+    <!--
     <section class="galerija-info">
         <div class="description-main">
             <h1>Delo z atleti</h1>
@@ -20,32 +40,32 @@
         <div class="img-galerija">
             <img src="../assets/camera.png" alt="" id="uploaded-image">
         </div>
-        <!-- Button to open modal -->
+        
         <button id="open-modal">Open Image Cropper</button>
     </section>
 
-    <!-- Modal Structure -->
+    
     <div id="cropper-modal">
         <div class="modal-content">
-            <!-- Close Button -->
+            
             <button id="close-modal">X</button>
             
-            <!-- Aspect Ratio Selection -->
+            
             <div class="aspect-ratio-options">
                 <label><input type="radio" name="aspect-ratio" value="4/3" checked> 4:3</label>
                 <label><input type="radio" name="aspect-ratio" value="3/4"> 3:4</label>
                 <label><input type="radio" name="aspect-ratio" value="1/1"> 1:1</label>
             </div>
             
-            <!-- Image to Crop -->
+            
             <img id="image-to-crop" src="../assets/camera.png" alt="Image to crop">
             
-            <!-- Button to Download Cropped Image -->
+           
             <button id="download-cropped">Download Cropped Image</button>
         </div>
     </div>
 
-    <!-- Cropper.js Script -->
+    
     <script src="https://cdn.jsdelivr.net/npm/cropperjs/dist/cropper.min.js"></script>
     <script>
         let cropper;
@@ -99,5 +119,7 @@
             link.click();
         });
     </script>
+-->
+    <?php include("footer.php") ?>
 </body>
 </html>
